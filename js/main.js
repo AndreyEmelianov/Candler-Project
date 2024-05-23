b6124b57bcbbef141625d71eb07de1b261412762
Andrey Emelianov <621077@mail.ru>
2024-05-23 21:18:48 +0300

    реализовал две секции со слайдерами, первая секция отзывы, вторая сертификаты, подключил swiper slider, реализовал разметку, стилизовал, сделал адапатив

--- js/main.js
+++ js/main.js
@@ -4,3 +4,34 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', () => {
   menuMobile.classList.toggle('menu--open');
 });
+
+const swiperFeedback = new Swiper('.feedback__slider', {
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiperCertificate = new Swiper('.certificates__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  breakpoints: {
+    500: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    360: {
+      slidesPerView: 1,
+    },
+  },
+});
