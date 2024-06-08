b4f09723859131f860e5607d03ba5d078e94fedc
Andrey Emelianov <621077@mail.ru>
2024-06-08 21:03:47 +0300

    реализовал секцию questions с аккордионом, сделал разметку, стилизовал, добавил обработчики событий, адаптив готов. Макет завершён

--- js/main.js
+++ js/main.js
@@ -35,3 +35,21 @@ const swiperCertificate = new Swiper('.certificates__slider', {
     },
   },
 });
+
+const accordion = document.querySelector('.accordion');
+const accordionTitles = document.querySelectorAll('.accordion__title');
+
+accordionTitles.forEach.call(accordionTitles, function (title) {
+  title.addEventListener('click', function () {
+    const currentText = title.parentElement.querySelector('.accordion__text');
+
+    title.classList.toggle('accordion__title--active');
+    currentText.classList.toggle('accordion__text--visible');
+
+    if (currentText.classList.contains('accordion__text--visible')) {
+      currentText.style.maxHeight = currentText.scrollHeight + 'px';
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  });
+});
